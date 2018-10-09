9fa7efc9eecb0572149119080b6412e3013d6c21
jinhaidi <haidi.jin@baodanyun-inc.com>
2018-10-09 10:22:24 +0800

    add canvas

--- admin/src/pages/login.vue
+++ admin/src/pages/login.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="login_page fillcontain">
+        <canvas ref="canvas"></canvas>
         <transition name="form-fade" mode="in-out">
             <section class="form_contianer" v-show="showLogin">
                 <div class="manage_tip">
@@ -36,8 +37,52 @@ export default {
       }
     }
   },
-  created() {
-
+  mounted() {
+    document.addEventListener('touchmove', function (e) {
+        e.preventDefault()
+    })
+    var c = this.$refs.canvas,
+        x = c.getContext('2d'),
+        pr = window.devicePixelRatio || 1,
+        w = window.innerWidth,
+        h = window.innerHeight,
+        f = 160,
+        q,
+        m = Math,
+        r = 100,
+        u = m.PI*2,
+        v = m.sin,
+        z = m.random
+    c.width = w*pr
+    c.height = h*pr
+    x.scale(pr, pr)
+    x.globalAlpha = 0.6
+    function i(){
+        x.clearRect(0,0,w,h)
+        q=[{x:0,y:h*.7+f},{x:0,y:h*.7-f}]
+        while(q[1].x<w+f) d(q[0], q[1])
+    }
+    function d(i,j){
+        x.beginPath()
+        x.moveTo(i.x, i.y)
+        x.lineTo(j.x, j.y)
+        var k = j.x + (z()*2-0.25)*f,
+            n = y(j.y)
+        x.lineTo(k, n)
+        x.closePath()
+        r-=u/-50
+        x.fillStyle = '#'+(v(r)*127+128<<16 | v(r+u/3)*127+128<<8 | v(r+u/3*2)*127+128).toString(16)
+        x.fill()
+        q[0] = q[1]
+        q[1] = {x:k,y:n}
+    }
+    function y(p){
+        var t = p + (z()*2-1.1)*f
+        return (t>h||t<0) ? y(p) : t
+    }
+    document.onclick = i
+    document.ontouchstart = i
+    i()
   },
   methods: {
     submit () {
@@ -57,6 +102,15 @@ export default {
   .el-loading-text, i {
     color: #fff;
   }
+}
+canvas {
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 0;
+    width: 100%;
+    height: 100%;
+    pointer-events: none;
 }
 .login_page {
   display: flex;
